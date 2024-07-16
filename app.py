276a85e82b0142a3e5ac661bd8e0302015830243
VerbalThree <verbalthree00@gmail.com>
2024-07-16 19:40:59 -0300

    changes

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+import sqlite3
 
 app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+db = sqlite3.connect("socialdata.db")
 
 @app.route("/")
 def login():
