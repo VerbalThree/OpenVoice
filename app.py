61b6a87173675d51d48e07ef49bf3d3c36e0d7e9
VerbalThree <verbalthree00@gmail.com>
2024-07-16 21:40:57 -0300

    changes

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, redirect, request, session
-from flask_session import Session
-from werkzeug.security import check_password_hash, generate_password_hash
+import os
+from flask import Flask, render_template, redirect, request, session # type: ignore
+from flask_session import Session # type: ignore
+from werkzeug.security import check_password_hash, generate_password_hash # type: ignore
 import sqlite3
-
+from parameters import apology, login_required
 app = Flask(__name__)
 
 # Configure session to use filesystem (instead of signed cookies)
@@ -14,5 +15,41 @@ Session(app)
 db = sqlite3.connect("socialdata.db")
 
 @app.route("/")
+@login_required
+def index():
+    return render_template("path-notes.html")
+
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("form-page.html")
+    """Log the user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (by submitting a form via POST, trying to log in)
+    if request.method == "POST":
+        
+        # Ensure the email was submitted
+        if not request.form.get("email"):
+            return apology("Email needed ", 403)
+        
+        # Ensure the password was submitted
+        elif not request.form.get("psw"):
+            return apology("Password needed", 403)
+        
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE email = ?", request.form.get("email"))
+
+        # Ensure that the username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("psw")):
+            return apology("Invalid email or password", 403)
+        
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+        return redirect("/")
+
+
+    # If the user clicked in a link (GET)
+    return render_template("login.html")
