52964198230683d2b6c24bf20a669c8b3fcf3781
Felipe Pereira <verbalthree00@gmail.com>
2024-08-29 12:04:04 +0000

    SPA Development

---
+++ OpenVoice-laravel/resources/components/ExampleComponent.vue
@@ -0,0 +1,14 @@
+<tempalte>
+    <div>
+        <h1>Vue</h1>
+        <h2>{{ title }}</h2>
+    </div>
+</tempalte>
+
+<script>
+export default {
+    setup() => ({
+        title: ' How to install Vue and Laravel'
+    })
+}
+</script>
