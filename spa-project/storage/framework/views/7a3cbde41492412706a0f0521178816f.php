d5606be915896d497e7d50396c74de8f0ca79106
VerbalThree <verbalthree00@gmail.com>
2024-09-01 11:48:18 -0300

    Changes

---
+++ spa-project/storage/framework/views/7a3cbde41492412706a0f0521178816f.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="app">hello</div>    
+    <script src="<?php echo e(asset('js/app.js')); ?>"></script>
+</body>
+</html><?php /**PATH /home/felipe/Desktop/spa-project/resources/views/welcome.blade.php ENDPATH**/ ?>
